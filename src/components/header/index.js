65eb683ed7dde1a6271b9f0d4c8d27109fad895a
Ebubekir-Tas <tas_ebubekir@yahoo.com>
2023-02-20 13:37:51 -0500

    capitalize winner

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { HeaderContainer, SearchPokemon, BattleButton } from './styles';
 import PropTypes from "prop-types";
 import usePokemonBattle from '../../hooks/usePokemonBattle';
+import { Typography } from '@mui/material';
 
 const Header = ({ pokemonFilter, setPokemonFilter, selectedPokemon }) => {
   const [winner, determineWinner] = usePokemonBattle();
@@ -17,7 +18,7 @@ const Header = ({ pokemonFilter, setPokemonFilter, selectedPokemon }) => {
     />
     <BattleButton variant="outlined" onClick={()=> determineWinner(selectedPokemon[0], selectedPokemon[1])}>Battle</BattleButton>
   </HeaderContainer>
-  <div>{winner && `Winner: ${winner}`}</div>
+  <Typography variant="h3">{winner && `Winner: ${winner.charAt(0).toUpperCase() + winner.slice(1)}`}</Typography>
   </>
 )};
 
